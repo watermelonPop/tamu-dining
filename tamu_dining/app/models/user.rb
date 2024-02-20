f222aa23d3e6f8d28943e50004b2645db25d01d0
boxkr <keeg9087@gmail.com>
2024-02-20 18:29:02 +0000

    good coverage

--- tamu_dining/app/models/user.rb
+++ tamu_dining/app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+    validates :credits, numericality: { only_integer: true, message: "must be a number" }
 end
--- tamu_dining/spec/controllers/users_controller_spec.rb
+++ tamu_dining/spec/controllers/users_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe UsersController, type: :controller do
         User.create(first_name: 'Lemon', last_name: 'Jello', uin: '463525785', email: 'lemonj@tamu.edu', credits: 0)
         User.create(first_name: 'Renee', last_name: 'Rapp', uin: '361543783', email: 'rr@tamu.edu', credits: 24)
         User.create(first_name: 'Shakira', last_name: 'Shakira', uin: '643971828', email: 'shakira@tamu.edu', credits: 44)
+        User.create(first_name:"no", last_name:"credits", uin:"098765432", email:"j@tamu.edu", credits: 0)
     end
 
     describe "GET #index" do
@@ -24,7 +25,7 @@ RSpec.describe UsersController, type: :controller do
         end
 
         it "returns a not found response for non-existing user" do
-            get :show, params: { uin: 'nonexistent' }
+            get :show, params: { uin: 'not_in_database' }
             expect(response).to have_http_status(:not_found)
         end
     end
@@ -56,6 +57,20 @@ RSpec.describe UsersController, type: :controller do
             expect(response).to have_http_status(:unprocessable_entity)
             expect(JSON.parse(response.body)['error']).to eq('UIN already exists in the database')
         end
+
+        it "returns err for non-numerical credits" do
+            # Create a user with the same UIN before attempting to create again
+            invalid_user_attributes = {
+            first_name: 'John',
+            last_name: 'Doe',
+            uin: '123456789',
+            email: 'johndoe@tamu.edu',
+            credits: "abc"
+            }
+            
+            post :create, params: { user: invalid_user_attributes }
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
     end
 
     describe "PATCH #update_credits" do
@@ -75,6 +90,29 @@ RSpec.describe UsersController, type: :controller do
             expect(response).to be_successful
             expect(user.credits).to eq(48)
             expect(JSON.parse(response.body)['message']).to eq('Credits updated successfully')
+        end
+
+        it "fails for an unexisting user" do
+            patch :update_credits, params: { uin: 'none', credits: 4 }
+            user = User.find_by(uin: 'none')
+            expect(response).to have_http_status(:not_found)
+        end
+    end
+
+    describe "PATCH user" do
+        it 'patch successfully' do
+
+            @user = user = User.find_by(first_name: 'no')
+            patch :update, params: { uin: @user.uin, user: { credits: 0 } }, as: :json
+            assert_response :success
+            @user.reload
+            assert_equal 0, @user.credits
+        end
+
+        it 'patch unsuccessfully' do
+            @user = user = User.find_by(first_name: 'no')
+            patch :update, params: { uin: @user.uin, user: {credits: '',} }, as: :json
+            expect(response).to have_http_status(:unprocessable_entity)
         end
     end
 
